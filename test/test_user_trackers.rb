e30c3c48480933d1a7bfdc6661de9031d922ac27
Camilo Barraza <acordeonlwebmaster@gmail.com>
2019-11-19 11:33:24 -0500

    remove test with minitest

--- test/test_user_trackers.rb
+++
@@ -1,9 +0,0 @@
-
-require 'minitest/autorun'
-require 'user_trackers'
-
-class UserTrackersTest < Minitest::Test
-  def test_basic
-    assert_equal UserTrackers.trackers, ['mixpanel','intercom','slack']
-  end
-end
