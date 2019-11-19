c08ae801162bb90c0c31284aa0ada8cc5489d24e
Camilo Barraza <acordeonlwebmaster@gmail.com>
2019-11-19 01:34:14 -0500

    update configure mixpanel template

--- lib/generators/templates/configure_mixpanel.rb
+++ lib/generators/templates/configure_mixpanel.rb
@@ -5,7 +5,6 @@ UserTrackers.configure_mixpanel do |config|
       '$first_name': user.first_name,
       '$last_name': user.last_name,
       '$email': user.email, 
-      '$test': 'test_value2' 
     }
   end
 
